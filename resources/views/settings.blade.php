c415feb8523aef0530f68c6326b4b53783d8677b
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-05-29 02:15:09 -0700

    Settings page

---
+++ resources/views/settings.blade.php
@@ -0,0 +1,22 @@
+@extends('ajax')
+
+<div class="container" id="content">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">User Settings</div>
+                <div class="panel-body">
+
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">API Authentication</div>
+                <div class="panel-body">
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens></passport-personal-access-tokens>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::get('/admin', 'HomeController@admin');
 Route::get('/albums/image/{album}', 'AlbumController@image');
 Route::get('/client/{url?}/{url2?}/{url3?}', 'ClientController')->name('client');
 Route::get('/home', 'HomeController@index');
+Route::get('/settings', function() {
+    return view('settings');
+})->name('settings');
 Route::get('/tracks/{track}/audio', 'TrackController@audio');
 Route::get('/tracks/queue', 'TrackController@queue')->name('tracks.queue');
 
