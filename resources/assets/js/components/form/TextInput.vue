21e23859d42d067553f618a6394c808522d2ba2a
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-07-21 01:56:12 -0700

    Form modularization work

---
+++ resources/assets/js/components/form/TextInput.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="form-group">
+        <label :for="name" class="col-md-4 control-label">{{ label }}</label>
+
+        <div class="col-md-6">
+            <input :id="name" type="text" class="form-control" :name="name" required autofocus>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['name', 'label']
+    }
+</script>
