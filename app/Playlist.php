4657a14f8aa1d309c8005cd6c2601a3061cf3a0f
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-07-23 22:33:38 -0700

    Minor controller fixes

--- app/Playlist.php
+++ app/Playlist.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Playlist extends Model
 {
-    //
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function tracks() {
+        return $this->belongsToMany('App\Track');
+    }
 }
