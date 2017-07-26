c4a8edab3ca0e9014c5d796b830220d58c4ae6ec
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-07-26 00:51:25 -0700

    Remove unnecessary required property

--- resources/assets/js/components/form/PasswordInput.vue
+++ resources/assets/js/components/form/PasswordInput.vue
@@ -3,7 +3,7 @@
         <label :for="name" class="col-md-4 control-label">{{ label }}</label>
 
         <div class="col-md-6">
-            <input :id="name" type="password" class="form-control" :name="name" :placeholder="hint" required autofocus>
+            <input :id="name" type="password" class="form-control" :name="name" :placeholder="hint" autofocus>
         </div>
     </div>
 </template>
