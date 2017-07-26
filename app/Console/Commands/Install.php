6ccffe5490891bee86b181958a567636ef255f08
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-07-26 00:50:19 -0700

    Create separate command for Passport setup

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -54,28 +54,7 @@ class Install extends Command
          */
         $this->call('migrate');
         $this->call('db:seed');
-
-        $path = $this->laravel->environmentFilePath();
-        $this->call('passport:install');
-
-        $this->info('Copying Passport password client settings...');
-        $client = \Illuminate\Support\Facades\DB::table('oauth_clients')->where('password_client', 1)->first();
-        /**
-         * This is the Laravel 5.4 method of replacing environment values which
-         * is more to date. It's quite complicated however, so it only remains
-         * here for reference purposes.
-         */
-        // file_put_contents($this->laravel->environmentFilePath(), preg_replace(
-        //     $this->keyReplacementPattern(),
-        //     'PASSWORD_CLIENT_ID='.$passwordClient->id,
-        //     file_get_contents($this->laravel->environmentFilePath())
-        // ));
-        file_put_contents($path, str_replace(
-            'PASSWORD_CLIENT_ID='.env('PASSWORD_CLIENT_ID'), 'PASSWORD_CLIENT_ID='.$client->id, file_get_contents($path)
-        ));
-        file_put_contents($path, str_replace(
-            'PASSWORD_CLIENT_SECRET='.env('PASSWORD_CLIENT_SECRET'), 'PASSWORD_CLIENT_SECRET='.$client->secret, file_get_contents($path)
-        ));
+        $this->call('install:oauth');
         // $this->call('passport:install', [
         //     'user' => 1, '--queue' => 'default'
         // ]);
