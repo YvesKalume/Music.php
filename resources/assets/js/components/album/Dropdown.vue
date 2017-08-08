bb86630dccd397f3770576cd56065d212daf62f9
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-08-08 00:46:59 -0700

    Dropdown fixes

--- resources/assets/js/components/album/Dropdown.vue
+++ resources/assets/js/components/album/Dropdown.vue
@@ -1,15 +1,36 @@
 <template>
     <ul class="dropdown-menu">
         <li><a href="#" v-on:click="setView('album-show', ['setAlbum', album.id])">View</a></li>
-        <li><a href="#" v-on:click="play(track)">Play</a></li>
-        <li><a href="#" v-if="checkAdmin()" :href="edit(album.id)" target="_blank">Edit</a></li>
-        <li><a href="#" v-on:click="push([track])">Queue</a></li>
-        <li><a href="#" v-on:click="deleteResource('albums', album.id)">Delete</a></li>
+        <li><a href="#" v-on:click="player.play(albumData.tracks)">Play</a></li>
+        <li><a href="#" v-if="checkAdmin()" :href="'/albums/' + album.id + '/edit'" target="_blank">Edit</a></li>
+        <li><a href="#" v-on:click="player.push(albumData.tracks, album.id)">Queue</a></li>
+        <li><a href="#" v-if="checkAdmin()" v-on:click="deleteResource('albums', album.id)">Delete</a></li>
     </ul>
 </template>
 
 <script>
+    import player from "../../player.js";
+
     export default {
+        data: function() {
+            return {
+                albumData: null,
+                player: player
+            }
+        },
+        mounted() {
+            $.get({
+                url: "/albums/" + this.album.id,
+                error: err => {
+                    // let newWindow = window.open("", "_blank");
+                    // newWindow.document.write(err.responseText);
+                    // newWindow.stop();
+                },
+                success: data => {
+                    this.albumData = data;
+                }
+            });
+        },
         props: ['album']
     }
 </script>
